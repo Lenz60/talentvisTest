9f15e5d5b1587f9f5a14d675a316201e42863ea1
Rafly Andrian <raflyandrian@gmail.com>
2024-04-19 22:41:51 +0700

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="flex flex-col border-2 border-green-400">
+      <div
+        class="flex flex-col justify-center items-center border-2 border-yellow-400"
+      >
+        <div class="flex flex-row">
+          <h1 class="text-[50px] font-medium text-primary pt-2">Green</h1>
+          <h1 class="text-[50px] font-medium text-neutral-content pt-2 pl-2">
+            Aquatic Shop
+          </h1>
+        </div>
+        <h3 class="p-2 text-md font-sans text-secondary">
+          Get your aquatic plants for your aquascape
+        </h3>
+      </div>
+      <div class="m-5 border-2 border-blue-400"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
